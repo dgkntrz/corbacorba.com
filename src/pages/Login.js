81af965a316113b71d7e436cacd40cb3cac90e7b
Doğukan Terzi <dogukanterzi@gmail.com>
2021-07-01 00:39:24 +0300

    commit

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,10 +1,21 @@
 import React, { useState } from 'react';
-import { TextField, Button } from '@material-ui/core';
+import { TextField, Button, Checkbox } from '@material-ui/core';
 import '../App.css'
 
 function Login() {
     const [userName, setUserName] = useState("");
     const [password, setPassword] = useState("");
+
+    const rememberMe = localStorage.getItem("remember");
+    
+    const [checked, setChecked] = useState(rememberMe == "yes"? true: false);
+    let defaultName = "";
+    let defaultPass = "";
+
+    if (rememberMe){
+        defaultName = localStorage.getItem("name");
+        defaultPass = localStorage.getItem("password");
+    }
 
     return (
         <div class="container" style={{ marginTop: "35vh" }}>
@@ -19,6 +30,7 @@ function Login() {
                     color="primary"
                     margin="dense"
                     id="name"
+                    defaultValue={defaultName}
                     label="Username"
                     type="email"
                     onChange={(e) => {
@@ -38,6 +50,7 @@ function Login() {
                     color="primary"
                     margin="dense"
                     id="name"
+                    defaultValue={defaultPass}
                     label="Password"
                     type="password"
                     onChange={(e) => {
@@ -46,10 +59,32 @@ function Login() {
 
                 />
             </div>
-            <a href="/forgotpassword">Forgot password?</a>
-            <div class="row" style={{ paddingTop: "1em" }}>
-                <Button variant="contained" color="primary">Log in</Button>
+            <div class="row"><Checkbox checked={checked} onClick={()=>{
+                if (checked){
+                    setChecked(false);
+                    localStorage.setItem("remember", "no");
+                }
+                else{
+                    setChecked(true);
+                    localStorage.setItem("remember", "yes");
+                }
+                }} color="primary"/>Remember me</div>
+            <div class="row" >
+                <Button variant="contained" color="primary" onClick={()=>{
+                    if (checked){
+                        localStorage.setItem("password", password);
+                        localStorage.setItem("name", userName);
+                    }
+                }}>Log in</Button>
             </div>
+            <div class="row" style={{ paddingTop: "0.5em" }}>
+                Don't have an account? <a href="/signup">Sign up!</a>
+            </div>
+            <div class="row" style={{ paddingTop: "0.5em" }}>
+                <a href="/forgotpassword">Forgot password?</a>
+            </div>
+            
+            
 
         </div>
     );
