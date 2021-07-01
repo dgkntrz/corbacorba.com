0f903374d67ee5a512e8cb6050e84b1e846b3ca9
Doğukan Terzi <dogukanterzi@gmail.com>
2021-07-01 13:00:30 +0300

    signup

--- src/Main.js
+++ src/Main.js
@@ -1,13 +1,17 @@
 import React from 'react';
 import { Switch, Route, BrowserRouter } from 'react-router-dom';
 import Login from './pages/Login';
+import Home from './pages/Home';
+import Signup from './pages/Signup'
 
 const Main = () => {
     localStorage.setItem("sel",0);
     return (
         <BrowserRouter>
             <Switch> {/* The Switch decides which component to show based on the current URL.*/}
+                <Route exact path='/' component={Home}></Route>
                 <Route exact path='/login' component={Login}></Route>
+                <Route exact path='/signup' component={Signup}></Route>
             </Switch>
         </BrowserRouter>
     );
--- src/pages/Signup.js
+++ src/pages/Signup.js
@@ -5,23 +5,14 @@ import { SHA256 } from 'crypto-js';
 
 function Login() {
     const [userName, setUserName] = useState("");
-    const [password, setPassword] = useState("");
-
-    const rememberMe = localStorage.getItem("remember");
-    
-    const [checked, setChecked] = useState(rememberMe == "yes"? true: false);
-    let defaultName = "";
-    let defaultPass = "";
-
-    if (rememberMe){
-        defaultName = localStorage.getItem("name");
-        defaultPass = localStorage.getItem("password");
-    }
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");  
+    const [checked, setChecked] = useState(false);
 
     return (
         <div class="container" style={{ marginTop: "30vh" }}>
             <div class="row">
-                <b style={{fontSize: "20px"}}>Log in</b>
+                <b style={{fontSize: "20px"}}>Sign up</b>
             </div>
             <div class="row">
                 <TextField
@@ -34,7 +25,6 @@ function Login() {
                     color="primary"
                     margin="dense"
                     id="name"
-                    defaultValue={defaultName}
                     label="Username"
                     type="email"
                     onChange={(e) => {
@@ -54,7 +44,25 @@ function Login() {
                     color="primary"
                     margin="dense"
                     id="name"
-                    defaultValue={defaultPass}
+                    label="E-mail"
+                    type="email"
+                    onChange={(e) => {
+                        setEmail(e.target.value);
+                    }}
+                    style={{ color: "blue" }}
+                />
+            </div>
+            <div class="row">
+                <TextField
+                    inputProps={{
+                        autocomplete: 'new-password',
+                        form: {
+                            autocomplete: 'off',
+                        },
+                    }}
+                    color="primary"
+                    margin="dense"
+                    id="name"
                     label="Password"
                     type="password"
                     onChange={(e) => {
@@ -63,7 +71,7 @@ function Login() {
 
                 />
             </div>
-            <div class="row"><Checkbox checked={checked} onClick={()=>{
+            <div class="row" style={{paddingTop:"0.5em"}}><Checkbox checked={checked} onClick={()=>{
                 if (checked){
                     setChecked(false);
                     localStorage.setItem("remember", "no");
@@ -74,6 +82,9 @@ function Login() {
                 }
                 }} color="primary"/>Remember me</div>
             <div class="row" >
+                By signing up, you agree to the <a href="/tos">Terms of Service</a> and  <a href="/tos">Privacy Policy</a>.
+            </div>  
+            <div class="row" style={{paddingTop: "1em"}}>
                 <Button variant="contained" color="primary" onClick={()=>{
                     if (checked){
                         localStorage.setItem("password", password);
@@ -81,17 +92,11 @@ function Login() {
                     }
                     const encryptedPassword = SHA256(password).toString();
   
-                }}>Log in</Button>
+                }}>Sign up</Button>
             </div>
-            <div class="row" style={{ paddingTop: "0.5em" }}>
-                Don't have an account? <a href="/signup">Sign up!</a>
-            </div>
-            <div class="row" style={{ paddingTop: "0.5em" }}>
-                <a href="/forgotpassword">Forgot password?</a>
-            </div>
-            
-            
-
+            <div class="row" style={{ paddingTop: "1em" }}>
+                Have an account? <a href="/signup">Log in!</a>
+            </div>  
         </div>
     );
 }
