401888fd787bd948edac0ed9aa62d8d45bfe55ec
Doğukan Terzi <dogukanterzi@gmail.com>
2021-07-03 15:46:15 +0300

    sign up

--- src/pages/Signup.js
+++ src/pages/Signup.js
@@ -10,6 +10,25 @@ function Login() {
     const [password, setPassword] = useState("");  
     const [checked, setChecked] = useState(false);
 
+    function signUpClick(){
+        if (checked){
+            localStorage.setItem("password", password);
+            localStorage.setItem("name", userName);
+        }
+        const encryptedPassword = SHA256(password).toString();
+        const params = {username: userName, password: encryptedPassword, email: email}
+        axios.post('http://localhost:8080/api/auth/register', {params})
+        .then(function (response) {
+            if (response.data.errorDto) {
+                // toast.error(response.data.errorDto.description)
+                console.log("signed up")
+            } else {
+                // toast.info("giriş başarılı")
+                console.log("damn")
+            }
+        })
+    }
+
     return (
         <div class="container" style={{ marginTop: "30vh" }}>
             <div class="row">
@@ -86,14 +105,7 @@ function Login() {
                 By signing up, you agree to the <a href="/tos">Terms of Service</a> and  <a href="/tos">Privacy Policy</a>.
             </div>  
             <div class="row" style={{paddingTop: "1em"}}>
-                <Button variant="contained" color="primary" onClick={()=>{
-                    if (checked){
-                        localStorage.setItem("password", password);
-                        localStorage.setItem("name", userName);
-                    }
-                    const encryptedPassword = SHA256(password).toString();
-  
-                }}>Sign up</Button>
+                <Button variant="contained" color="primary" onClick={signUpClick}>Sign up</Button>
             </div>
             <div class="row" style={{ paddingTop: "1em" }}>
                 Have an account? <a href="/login">Log in!</a>
